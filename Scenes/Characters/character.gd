e1b732ce0c66c624b5f6468838afe7bc4b56a875
Ruslan HUAWEI D16 <ruslantol09@gmail.com>
2025-11-12 10:26:46 +0500

    0.0.0.1 OOP beginning

---
+++ Scenes/Characters/character.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+@export var speed = null
+@export var health = null
+@export var stamina = null
+@export var crit_chance = null
+@export var acelerate = null
+@export var id = null
